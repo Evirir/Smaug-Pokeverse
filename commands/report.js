4a159e9471be6d81476af9c0b9a7316565d7b335
Evirir <46616851+Evirir@users.noreply.github.com>
2019-04-13 18:54:19 +0800

    Update report.js

--- commands/report.js
+++ commands/report.js
@@ -8,7 +8,7 @@ module.exports = {
 	usage: `[message]`,
 
 	execute(message, args) {
-		message.channel.send(`A dragon courier flies towards you, and carefully writes down your message...\nMessage sent to Evirir!`);
-		message.channel.users.get(dragID).send(`A dragon courier brought a message to you!\n${message.author.name} said: ${message.content}`);
+		message.channel.send(`A dragon courier flies towards you, and carefully writes down your message, and flies away...\n**Message sent to Evirir!**`);
+		message.client.users.get(dragID).send(`A dragon courier brought a message to you!\n${message.author.name} said: ${message.content}`);
 	}
 };
