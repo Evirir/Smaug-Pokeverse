2fc6b0e9f8a9aba28dc173aa95adf07c3f545f09
Evirir <gojunxing@gmail.com>
2019-05-09 01:54:09 +0800

    Fix daily.js crashing issue (missing comma after wip:true)

--- hoardCommands/daily.js
+++ hoardCommands/daily.js
@@ -15,7 +15,7 @@ module.exports = {
 	description: `Claims your daily reward`,
     aliases: ['d'],
     hoard: true,
-    wip: true
+    wip: true,
 
 	execute (message, args) {
         const user = message.author.id;
