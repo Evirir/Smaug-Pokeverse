9b76c87ffb1d2d5d6cb04050716ed008304d16aa
Evirir <gojunxing@gmail.com>
2019-05-14 00:50:32 +0800

    Changed color of embed

--- commandsUtil/invite.js
+++ commandsUtil/invite.js
@@ -9,7 +9,7 @@ module.exports = {
 		let embed = new Discord.RichEmbed()
 		.setTitle(`Interested to have me in your server? Chant this long, blue, magical spell while worshipping the god of the dragons!`)
 		.setDescription(`<https://discordapp.com/oauth2/authorize?&client_id=557528854147629056&scope=bot&permissions=67434561>`)
-		.setColor('BLUE')
+		.setColor('GOLD')
 		.setFooter(`P.S. Nah jk, just click on the link`);
 
 		message.channel.send(embed);
