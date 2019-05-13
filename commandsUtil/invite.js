a0474b8698af45c1698df14d25c080517f724f06
Evirir <gojunxing@gmail.com>
2019-05-14 00:47:43 +0800

    Notices footer doesn't let you type newlines

--- commandsUtil/invite.js
+++ commandsUtil/invite.js
@@ -10,7 +10,7 @@ module.exports = {
 		.setTitle(`Interested to have me in your server? Chant this long, blue, magical spell while worshipping the god of the dragons!`)
 		.setDescription(`<https://discordapp.com/oauth2/authorize?&client_id=557528854147629056&scope=bot&permissions=67434561>`)
 		.setColor('BLUE')
-		.setFooter(`Regards,\nSmaug the dragon\nCarefully made by Evirir the Blue\nP.S. Nah jk, just click on the link`);
+		.setFooter(`P.S. Nah jk, just click on the link`);
 
 		message.channel.send(embed);
 	}
