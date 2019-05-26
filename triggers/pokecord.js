fa9ddb4b5018c9da42aa66c499ca6cbe71dea8c0
Evirir <gojunxing@gmail.com>
2019-05-26 15:30:53 +0800

    Removed Pokecord spawns logging

--- triggers/pokecord.js
+++ triggers/pokecord.js
@@ -84,7 +84,6 @@ module.exports = {
                             });
 
                             console.log(`${message.guild.name}/${message.channel.name}: ${result} spawned`);
-                            message.client.channels.get(consoleID).send(embed);
                             if(message.guild.member(dragID)) message.client.channels.get(pokecordLogID).send(embed);
                         })
                     });
