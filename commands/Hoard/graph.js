513146063f0b9ed2fbda13cd22e1c173b03cd502
Evirir <gojunxing@gmail.com>
2019-06-02 23:55:01 +0800

    Added graph function

--- commands/Hoard/graph.js
+++ commands/Hoard/graph.js
@@ -1,13 +1,26 @@
 const Discord = require('discord.js');
-const GraphServer = require('../../models/graphUser.js');
+const GraphServer = require('../../models/graphServer.js');
 
 module.exports = {
 	name: 'graph',
 	description: `Shows the graph in the server.`,
-    aliases: ['g'],
-    wip: true,
+    aliases: ['gr'],
 
 	async execute(message, args, prefix){
+		let graphServer = await GraphServer.findOne({serverID: message.guild.id}).catch(err => console.log(err));
+		if(!graphServer) return console.log(`graph.js: No graphServer data found.`);
 
+		const currentNode = graphServer.graphUsers.get(message.author.id)
+
+		graphServer.adj.get(currentNode).sort();
+		const neighbours = graphServer.adj.get(currentNode);
+
+		const embed = new Discord.RichEmbed()
+		.setColor(`GOLD`)
+		.setAuthor(message.author.username, message.author.displayAvatarURL)
+		.setTitle(`${message.guild.name}'s graph`)
+		.setDescription(`Your neighbours are: \`${neighbours.join(', ')}\``)
+
+		message.channel.send(embed);
 	}
 };
