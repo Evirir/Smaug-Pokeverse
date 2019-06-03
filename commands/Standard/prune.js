6b915b66025041e2f34bab8b7e0e7e94370ed0b9
Evirir <gojunxing@gmail.com>
2019-06-03 09:47:25 +0800

    Code cleanup

--- commands/Standard/prune.js
+++ commands/Standard/prune.js
@@ -1,5 +1,3 @@
-const {dragID} = require(`../../specificData/users.json`);
-
 module.exports = {
 	name: 'prune',
 	description: `Deletes some number of latest messages (members with \`MANAGE_MESSAGES\` permission only).`,
@@ -18,7 +16,7 @@ module.exports = {
 			return message.reply('please input a number between 1 and 99.');
 		}
 
-		message.channel.bulkDelete(amount+1, true).catch(err => {
+		message.channel.bulkDelete(amount + 1, true).catch(err => {
 			console.error(err);
 			return message.reply('there was an error trying to prune messages in this channel! Perhaps the messages are more than 2 weeks old.');
 		});
