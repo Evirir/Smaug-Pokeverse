ff4bff8b4c462dca7b2ee0214f7fce69c0d81da8
Evirir <gojunxing@gmail.com>
2019-06-08 20:59:15 +0800

    Fixed intro crashing issue and added move.js and edited no args msg

--- commands/Hoard/move.js
+++ commands/Hoard/move.js
@@ -1,4 +1,5 @@
 const Discord = require('discord.js');
+const GraphUser = require('../../models/graphUser.js');
 const GraphServer = require('../../models/graphServer.js');
 
 module.exports = {
@@ -10,15 +11,23 @@ module.exports = {
 	async execute(message, args, prefix){
 		let graphServer = await GraphServer.findOne({serverID: message.guild.id}).catch(err => console.log(err));
 		if(!graphServer) return console.log(`move.js: No graphServer data found.`);
+		let graphUser = await GraphUser.findOne({userID: message.author.id}).catch(err => console.log(err));
+		if(!graphUser) return console.log(`move.js: No graphUser data found.`);
 
 		const currentNode = graphServer.graphUsers.get(message.author.id);
-        const targetNode = args[0];
+        const targetNode = parseInt(args[0]);
 
-		const neighbours = graphServer.adj.get(currentNode);
+		const targetEdge = graphServer.adj.get(currentNode).find(e => e.v === targetNode);
 
-        if(!neighbours.includes(targetNode)) return message.reply(`there's no existing edge to that node.`);
+        if(!targetEdge) return message.reply(`there's no existing edge to that node.`);
+		if(graphUser
 
-        const newNeighbours = graphServer.adj.get(targetNode);
+		graphUser.energy -= targetEdge.w;
+		graphServer.graphUsers.set(message.author.id, targetNode);
+		graphServer.nodeUsers[
+
+        await graphServer.save().catch(err => console.log(err));
+		await graphUser.save().catch(err => console.log(err));
 
 		const embed = new Discord.RichEmbed()
 		.setColor(`GOLD`)
--- commands/Standard/intro.js
+++ commands/Standard/intro.js
@@ -1,17 +1,17 @@
 const Discord = require(`discord.js`);
+const {cute} = require('../../imageLinks.json');
 
 module.exports = {
 	name: 'intro',
 	description: `Introduces myself briefly, in the best way o.=.o`,
     aliases: [`smaug`,`death`,`fire`],
 
-	execute(message, args){
-
-		const Embed = new Discord.RichEmbed()
+	execute(message, args, prefix){
+		const embed = new Discord.RichEmbed()
 			.setTitle(`***I'M FIRE, I'M DEATH!!!***`)
-			.attachFiles([`images/cute/cute (1).gif`])
-			.setImage(`attachment://cute (1).gif`);
+			.setColor('GREEN')
+			.setImage(dragon[0]);
 
-	    message.channel.send(Embed);
+	    message.channel.send(embed);
 	}
 };
