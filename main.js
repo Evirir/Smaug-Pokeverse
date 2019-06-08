b3394011b5add618cdd6be2f7a6815b659074946
Evirir <gojunxing@gmail.com>
2019-06-08 21:21:03 +0800

    Fixed stupid typo at main.js: line 87

--- main.js
+++ main.js
@@ -84,7 +84,7 @@ client.on('message', async message => {
 		return message.channel.send(`This command is only available to developers.`);
 	}
 	if(command.args && !args.length){
-		return client.commnads.get('help').execute(message, args, prefix);
+		return client.commands.get('help').execute(message, args, prefix);
 	}
 
 	if(!cooldowns.has(command.name) && command.cd) cooldowns.set(command.name, new Discord.Collection());
