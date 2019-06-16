8e7bb0ad50bb437d59ff15925addb94d03b05d7a
Evirir <gojunxing@gmail.com>
2019-06-16 18:41:16 +0800

    Edited invite link descs

--- commands/Utility/invite.js
+++ commands/Utility/invite.js
@@ -7,14 +7,13 @@ module.exports = {
 
 	async execute(message, args) {
 		let embed = new Discord.RichEmbed()
-		.setTitle(`Interested to have me in your server? Chant this blue, magical spell while worshipping the dragon god.`)
+		.setTitle(`Interested to have me in your server? Chant one of these blue, magical spells while worshipping the dragon god.`)
 		.setDescription(
-`[Flaargle Fla Gengubble (With admin perms)](${inviteLink} "See note for info")
-[Rarwrr Ghrrr (No admin ver)](${inviteLinkNoAdmin})
+`[**Flaargle Fla Gengubble** (With admin perms)](${inviteLink} "See note for info")
+[**Rarwrr Ghrrr** (No admin ver)](${inviteLinkNoAdmin})
 
-NOTE: The Raider Lock will NOT work if you do not give Smaug admin permmissions.
-If you can't give Smaug the admin permission, you can give Smaug the 'Manage Channel Permissions' permission in **every** channel where Raiders may spawn (Right-click channel -> Edit Channel -> Permissions).
-NOTE 2: You must use \`,,rs on\` to enable the Raider Lock and add the roles you want to lock with \`,,rs role add [role]\`. See \`,,help rs\` for usage.`)
+NOTE: The Raider Lock will NOT work if you do not give Smaug the admin permission.
+NOTE 2: You must use \`,,rs on\` to enable the Raider Lock and add the roles you want to lock. See \`,,help rs\` for help.`)
 		.setColor('GOLD');
 
 		message.channel.send(embed);
